396e2ffb1dd22912810b1234347e1bc24441429f
Flavia Sipres <sipresflavia@gmail.com>
2023-04-19 00:08:09 -0300

    Import subfunctions and packages to Extract_Spotify_Data function

---
+++ case_2/Orchestrator.py
@@ -0,0 +1,11 @@
+import datetime
+import pandas as pd
+
+
+from helpers import (
+    get_credentials_from_secret_manager,
+    get_access_token_spotify,
+    search_for_podcasts_spotify,
+    extract_episodes_from_show_spotify
+)
+
